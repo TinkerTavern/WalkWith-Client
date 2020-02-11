6ad11fe62c18fa568255baa4263fd14b1bb750d2
mrcool76 <mwc38@bath.ac.uk>
2020-02-11 11:54:40 +0000

    i like potatos

--- app/src/main/java/com/example/walkwith/MapsActivity.java
+++ app/src/main/java/com/example/walkwith/MapsActivity.java
@@ -12,7 +12,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    //POTATO
     private GoogleMap mMap;
 
     @Override
