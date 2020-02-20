13662ae28b666682e986aa748ca4a14b04b8aad1
Zhivko <zhivko.nenchev@gmail.com>
2020-02-20 15:56:39 +0000

    Supposedly UI working

---
+++ app/src/main/java/com/example/walkwith/FocusView.java
@@ -0,0 +1,15 @@
+package com.example.walkwith;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FocusView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_focus_view);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
