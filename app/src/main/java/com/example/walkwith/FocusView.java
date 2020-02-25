743fedd163753de8e03d444b39b957ec7138ec8b
j_ewuare <jerryiyi13@gmail.com>
2020-02-25 11:59:23 +0000

    Focus View Display Map and User Location on Map

--- app/src/main/java/com/example/walkwith/FocusView.java
+++ app/src/main/java/com/example/walkwith/FocusView.java
@@ -1,15 +1,61 @@
 package com.example.walkwith;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class FocusView extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.util.Objects;
+
+public class FocusView extends FragmentActivity implements GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener, OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_focus_view);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        try {
+            Objects.requireNonNull(mapFragment).getMapAsync(this);
+        }
+        catch (NullPointerException e) {
+            alertDialog("Map loading error", e.getMessage());
+        }
+    }
+
+    public void onMapReady(GoogleMap map){
+        mMap = map;
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+    }
+
+    private void alertDialog(String map_loading_error, String message) {
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
