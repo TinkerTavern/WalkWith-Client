e36ae446561b4cb9724fdeaf2584d81635729ed0
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2020-03-09 20:08:04 +0000

    Fixed some warnings

--- app/src/main/java/com/example/walkwith/SettingsActivity.java
+++ app/src/main/java/com/example/walkwith/SettingsActivity.java
@@ -10,7 +10,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.settings_activity);
 //        getSupportFragmentManager()
 //                .beginTransaction()
