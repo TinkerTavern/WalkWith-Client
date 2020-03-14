1dd8a316fc5760edb66a7fcf22db36c4e2e5ffb4
Tom L <lancaster.c.tom@gmail.com>
2020-03-14 18:35:27 +0000

    Remember Me button working (Now need to do log out)

--- app/src/main/java/com/example/walkwith/LoginActivity.java
+++ app/src/main/java/com/example/walkwith/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.walkwith;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.android.volley.Request;
@@ -14,7 +15,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -40,6 +43,28 @@ public class LoginActivity extends AppCompatActivity {
         final Button loginButton = findViewById(R.id.login);
         final Button createAccountButton = findViewById(R.id.createAccount);
         final Button forgotPasswordButton = findViewById(R.id.forgotPassword);
+        final Switch rememberMe = findViewById(R.id.rememberMe);
+
+        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+        String isRemember = preferences.getString("remember","");
+        if (isRemember.equals("true")) {
+            String email = preferences.getString("email","");
+            successfulLogin(email);
+        }
+
+        rememberMe.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = preferences.edit();
+            if (buttonView.isChecked()) {
+                editor.putString("remember", "true");
+                Toast.makeText(getApplicationContext(), "Login details will be remembered",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                editor.putString("remember", "false");
+                Toast.makeText(getApplicationContext(), "Will not remember login details",
+                        Toast.LENGTH_SHORT).show();
+            }
+            editor.apply();
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) { // TODO: (Re)implement loading icon for screen
@@ -78,10 +103,10 @@ public class LoginActivity extends AppCompatActivity {
                 @Override
                 public void onResponse(JSONObject response) {
                     try {
-                        validateLogin((String) response.get("result"), email);
+                        validateLogin((String) response.get("result"));
                         // Put the things you want to happen upon success
                     } catch (JSONException e) {
-                         validateLogin(e.getMessage(), email);
+                         validateLogin(e.getMessage());
                         // Put the error here
                     }
                 }
@@ -89,7 +114,7 @@ public class LoginActivity extends AppCompatActivity {
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     // put the error here
-                     validateLogin(error.getMessage(), email);
+                     validateLogin(error.getMessage());
                 }
             });
             // Add the request to the RequestQueue.
@@ -101,18 +126,31 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void validateLogin(String response, String email) {
+    private void validateLogin(String response) {
+        String email = usernameEditText.getText().toString();
         if (response.equals("True")) {
-            Toast.makeText(this, "Welcome, " + usernameEditText.getText().toString(),
+            Toast.makeText(this, "Welcome, " + email,
                     Toast.LENGTH_SHORT).show();
-            Intent myIntent = new Intent(LoginActivity.this, MainMenu.class);
-            // TODO: Pass in user information into screen
-            getUserInfo(email);
-            finish();
-            LoginActivity.this.startActivity(myIntent);
+            SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+            String isRemembered = preferences.getString("remember", "");
+
+            if (isRemembered.equals("true")) { // So to save login details for the future
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("email", email);
+                editor.apply();
+            }
+            successfulLogin(email);
         }
         else
             Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
+    }
+
+    private void successfulLogin(String email) {
+        Intent myIntent = new Intent(LoginActivity.this, MainMenu.class);
+        // TODO: Pass in user information into screen
+        getUserInfo(email);
+        finish();
+        LoginActivity.this.startActivity(myIntent);
     }
 
     private void getUserInfo(String email) {
