b58acc59113b01fba11c790a6c28f0da409243f0
Tom L <lancaster.c.tom@gmail.com>
2020-03-20 05:16:34 +0000

    Trusted contacts shows correct information and users can be clicked

--- app/src/main/java/com/example/walkwith/ContactInfo.java
+++ app/src/main/java/com/example/walkwith/ContactInfo.java
@@ -3,6 +3,7 @@ package com.example.walkwith;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ContactInfo extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class ContactInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_info);
+        setLabels();
+    }
+
+    private void setLabels() {
+        final TextView emailVal = findViewById(R.id.emailVal);
+        final TextView firstnameVal = findViewById(R.id.firstnameVal);
+        final TextView surnameVal = findViewById(R.id.surnameVal);
+        final TextView phoneNumVal = findViewById(R.id.phoneNumVal);
+        emailVal.setText(getIntent().getStringExtra("email"));
+        firstnameVal.setText(getIntent().getStringExtra("firstName"));
+        surnameVal.setText(getIntent().getStringExtra("surname"));
+        phoneNumVal.setText(getIntent().getStringExtra("phoneNum"));
     }
 }
--- app/src/main/java/com/example/walkwith/TrustedContactList.java
+++ app/src/main/java/com/example/walkwith/TrustedContactList.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -33,23 +47,8 @@ public class TrustedContactList extends AppCompatActivity implements MyRecyclerV
             }
         });
 
-        Button removeTC = findViewById(R.id.removeTCButton);
-
-        removeTC.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                onTCButtonClick(view);
-            }
-        });
-
-
         //data to populate the RecyclerView with
-        trustedContactNames.add("Tom");
-        trustedContactNames.add("Elora");
-        trustedContactNames.add("Matt");
-        trustedContactNames.add("Matas");
-        trustedContactNames.add("Jerry");
-        trustedContactNames.add("Zhivko");
+        getTrustedContacts();
 
         RecyclerView recyclerView = findViewById(R.id.TrustedContacts);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -59,71 +58,101 @@ public class TrustedContactList extends AppCompatActivity implements MyRecyclerV
 
     }
 
+    private void getTrustedContacts() {
+        trustedContactNames.addAll(AccountInfo.getFriendsList());
+    }
+
     @Override
     public void onItemClick(View view, int position) {
-
-        //Toast.makeText(this, "You deleted " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
         nameIndex = position;
-        //trustedContactNames.remove(position);
-        //adapter.notifyItemRemoved(position);
-        Intent openInfo = new Intent(this, ContactInfo.class);
-        startActivity(openInfo);
+        String email = trustedContactNames.get(position);
+        getUserInfoPOST(email);
+    }
+
+    private void getUserInfoPOST(String email) { // TODO Make this a general function
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = getResources().getString(R.string.server_ip) + "getFriendInfo";
+        try {
+            JSONObject jsonBody = new JSONObject();
+
+            jsonBody.put("email", email);
+
+            JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        String result = (String) response.get("result");
+                        if (result.equals("True")) {
+                            String tcEmail = (String) response.get("email");
+                            String firstName = (String) response.get("firstName");
+                            String surname = (String) response.get("lastName");
+                            String phoneNum = (String) response.get("phoneNum");
+                            Intent openInfo = new Intent(getApplicationContext(), ContactInfo.class);
+                            openInfo.putExtra("email", email);
+                            openInfo.putExtra("firstName", firstName);
+                            openInfo.putExtra("surname", surname);
+                            openInfo.putExtra("phoneNum", phoneNum);
+                            startActivity(openInfo);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Error in retrieving " +
+                                    "details, try again soon", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), "Error in retrieving " +
+                                "details, try again soon", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Error in retrieving " +
+                            "details, try again soon", Toast.LENGTH_SHORT).show();
+                }
+            });
+            queue.add(jsonObject);
+
+        } catch (
+                JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addContactPopup() {
+        android.app.AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Contact");
+        builder.setMessage("Enter the email of the contact you wish to add");
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Add Contact", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addContact(input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 
     public void onButtonClick(View view){
-//        final AlertDialog.Builder alert = new AlertDialog.Builder(TrustedContactList.this);
-//        final View v = getLayoutInflater().inflate(R.layout.contact_dialog_layout, null);
-//
-//
-//        EditText edit_FirstName =  v.findViewById(R.id.edit_FirstName);
-//        EditText edit_Surname =  v.findViewById(R.id.edit_Surname);
-//        EditText edit_Email =  v.findViewById(R.id.edit_email);
-//        Button button_submit = v.findViewById(R.id.button_submit);
-//
-//        alert.setView(v);
-//
-//
-//        final AlertDialog alertDialog = alert.create();
-//
-//
-//
-//        button_submit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String FirstName = edit_FirstName.getText().toString();
-//                String Surname = edit_Surname.getText().toString();
-//                String Email = edit_Email.getText().toString();
-//                insertSingleItem(FirstName);
-//                alertDialog.dismiss();
-//            }
-//        });
-//        alert.show();
+        addContactPopup();
     }
 
-    private void insertSingleItem(String name){
+    private void addContact(String name){
         String item = name;
         int insertIndex = 0;
         trustedContactNames.add(insertIndex, item);
         adapter.notifyItemInserted(insertIndex);
-    }
-
-    public void onTCButtonClick(View view){
-        removeSingleItem();
-    }
-
-    private void removeSingleItem(){ // Changed this so it removes the first value all the time
-        // I think I see what the intention was here, but realistically the removal should be done in
-        // The contactInfo activity
-//        openList();
-        if (trustedContactNames.size() > 0) {
-            Toast.makeText(this, "You deleted " + adapter.getItem(0) +
-                    " on row number " + 0, Toast.LENGTH_SHORT).show();
-            trustedContactNames.remove(0);
-            adapter.notifyItemRemoved(0);
-
-        }
-        else
-            Toast.makeText(this, "Nothing to remove", Toast.LENGTH_SHORT).show();
-
     }
 }
